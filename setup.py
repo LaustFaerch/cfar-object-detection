cd0addb25662e05b48e712dd859d9d8b364377aa
LaustFaerch <laust.faerch@gmail.com>
2021-03-19 13:29:33 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cfar_filters',
+    description='Selection of SAR CFAR functions',
+    author='lafa',
+    author_email='lfa027@uit.no',
+    packages=find_packages(),
+    python_requires='>=3.8',
+    use_scm_version=[],
+    setup_requires=[
+        'numpy',
+        'pandas',
+    ],
+    install_requires=[]
+)
