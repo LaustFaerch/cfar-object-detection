d4831d0d3dea2f341587cbd6f88571485581b93d
LaustFaerch <laust.faerch@gmail.com>
2021-12-15 14:21:48 +0100

    major update

---
+++ cfar_filters/dpolrad.py
@@ -0,0 +1,22 @@
+import numpy as np
+from rs_utils.sar_functions import db2in
+from .fast_functions import fast_edge_mean, fast_center_mean
+
+
+def transform(image, mask=0, pfa=1e-6):
+
+    # if no mask is given, assume all pixels are valid
+    if np.all(mask == 0):
+        mask = np.ones_like(image[0, ...]) > 0
+    
+    eps = db2in(-100)
+    
+    HV_test = fast_center_mean(image[1,...],)
+    HV_train = fast_edge_mean(image[1,...])
+    HH_train = fast_edge_mean(image[0,...])
+    
+    Δ = (HV_test-HV_train)/(HH_train+eps) > HV_test
+    
+    detection_image = (Δ*image[1,...]**2+eps)
+    
+    return detection_image
---
+++ cfar_filters/lognormal.py
@@ -0,0 +1,39 @@
+import warnings
+import numpy as np
+from scipy.special import erf
+from scipy.optimize import minimize
+from rs_utils.sar_functions import smells_like
+from .fast_functions import fast_edge_mean, fast_edge_std
+
+
+def _gaussian_pfa(t):
+    return 0.5-0.5*erf(t/np.sqrt(2))
+
+def _gaussian_pfa_minimization(x, pfa):
+    return np.abs(_gaussian_pfa(x)-pfa)
+
+def _find_gaussian_multiplier(pfa):
+    res = minimize(_gaussian_pfa_minimization, 5, args=(pfa), method='Nelder-Mead', tol=1e-6)
+    return res.x[0]
+
+def detector(image, mask=0, pfa=1e-6):
+
+    # if no mask is given, assume all pixels are valid
+    if np.all(mask == 0):
+        mask = np.ones_like(image[0, ...]) > 0
+    
+    # check if the image format
+    if smells_like(image) != 'decibel':
+        warnings.warn(f'Input image should be in decibel scale. Image smells like {smells_like(image)}',
+                      category=UserWarning)
+
+    image = image.squeeze()
+    # Define filter parameters
+    std_dev_multiplier = _find_gaussian_multiplier(pfa)
+
+    edge_mean = fast_edge_mean(image)
+    egde_std = fast_edge_std(image)
+
+    outliers = image>(edge_mean+std_dev_multiplier*egde_std)
+    
+    return outliers
