eb69c0c9df8a91a15bb55f1456278b95074b815c
LaustFaerch <laust.faerch@gmail.com>
2021-12-16 11:29:19 +0100

    :bug:

--- cfar_filters/dpolrad.py
+++ cfar_filters/dpolrad.py
@@ -12,12 +12,12 @@ def transform(image, mask=0):
     eps = db2in(-100)
 
     #  HV_test = fast_center_mean(image[1, ...], mask)
-    HV_test = image[1, ...]  # just testing what happens now
+    HV_test = image[1, ...]  # this appears better than ^that
     HV_train = fast_edge_mean(image[1, ...], mask)
     HH_train = fast_edge_mean(image[0, ...], mask)
 
-    Δ = (HV_test - HV_train) / (HH_train + eps) > HV_test
+    Δ = (HV_test - HV_train) / (HH_train + eps)
 
-    detection_image = (Δ * image[1, ...]**2 + eps)
+    detection_image = (Δ * HV_test)
 
     return detection_image
