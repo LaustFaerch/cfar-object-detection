4a3abcd91c341d3b3047c50d25f795f05ee0a932
LaustFaerch <laust.faerch@gmail.com>
2022-11-15 11:17:00 +0100

    dpolrad now using gen-gamma pdf

--- cfar_filters/dpolrad.py
+++ cfar_filters/dpolrad.py
@@ -1,18 +1,21 @@
 import warnings
+import scipy
 import numpy as np
 from .utils import smells_like, mask_edges
-from .fast_functions import fast_inner_mean, fast_outer_mean, fast_edge_mean
+from .fast_functions import test_window, train_window
 
+def _gengamma_minimize(t, gengamma_params, pde):
+    return np.abs(scipy.stats.gengamma.cdf(t, *gengamma_params) - pde)
 
-def transform(image, mask=0, version='original'):
+def transform(image, mask=0):
     """
     Dual-Pol Ratio Anomaly Detector Transformation (DPolRAD)
-    Based on the following paper:
+    this function implements equation 2 in the paper
     A. Marino, W. Dierking, and C. Wesche,
     “A Depolarization Ratio Anomaly Detector to Identify Icebergs in Sea Ice Using Dual-Polarization SAR Images,”
     IEEE Trans. Geosci. Remote Sens., vol. 54, no. 9, pp. 5602-5615, 2016, doi: 10.1109/TGRS.2016.2569450.
 
-    Specifically, this function implements equation 2 in the paper.
+    The test and training window are 3x3, 57x57 pixels respectively
 
     Parameters:
     ----------
@@ -25,6 +28,46 @@ def transform(image, mask=0, version='original'):
     ----------
     transform : numpy.ndarray(float32) (X,Y)
         DPolRad Transform
+    """
+
+    HV_target = test_window(image[1, ...], mask)  # test window
+    HV_clutter = train_window(image[1, ...], mask)  # train window
+    HH_clutter = train_window(image[0, ...], mask)  # train window
+
+    HH_clutter = np.where(HH_clutter == 0, np.nan, HH_clutter)  # dont divide by 0
+    transform = (HV_target - HV_clutter) / (HH_clutter)
+
+    return transform
+
+def detector(image, mask=0, pfa=1e-12):
+    """
+    HV Intensity Dual-Pol Ratio Anomaly Detector (IDPolRAD)
+
+    The detector is based on the transform suggested in the paper:
+    A. Marino, W. Dierking, and C. Wesche,
+    “A Depolarization Ratio Anomaly Detector to Identify Icebergs in Sea Ice Using Dual-Polarization SAR Images,”
+    IEEE Trans. Geosci. Remote Sens., vol. 54, no. 9, pp. 5602-5615, 2016, doi: 10.1109/TGRS.2016.2569450.
+
+    Outliers are detected in the transform using the method outlined in this paper:
+    I. H. Soldal, W. Dierking, A. A. Korosov, and A. Marino,
+    “Automatic detection of small icebergs in fast ice using satellite Wide-Swath SAR images,”
+    Remote Sens., vol. 11, no. 7, pp. 1-24, 2019, doi: 10.3390/rs11070806.
+
+    Specifically, we are applying a global CFAR detector based on the generalized gamma pdf (Table 10 in Soldal)
+
+    Parameters:
+    ----------
+    image : numpy.ndarray(float32) (2,X,Y)
+        SAR image in linear intensity format
+    mask : numpy.ndarray(bool) (X,Y)
+        Mask for the image.
+    pfa : float
+        Probability of false alarm. Should be somewhere between 0-1
+
+    Returns:
+    ----------
+    outliers : numpy.ndarray(bool) (X,Y)
+        Binary outlier classification
     """
 
     # if no mask is given, assume all pixels are valid
@@ -49,19 +92,25 @@ def transform(image, mask=0, version='original'):
         warnings.warn(f'Input image should be in intensity scale. Image smells like {smells_like(image)}',
                       category=UserWarning)
 
-    if version == 'original':
-        HV_target = fast_inner_mean(image[1, ...], mask)  # test window
-        HV_clutter = fast_outer_mean(image[1, ...], mask)  # train window
-        HH_clutter = fast_outer_mean(image[0, ...], mask)  # train window
+    # calculate the dpolrad transform
+    dpolrad = transform(image, mask=mask)
+    # multiply with HV to get contrast enhancement
+    idpolrad = image[1, ...] * dpolrad
+    # if dpolrad is negative, Idpolrad is set to zero (A. Marino Sec. IV C)
+    idpolrad = np.where(dpolrad < 0, 0, idpolrad)
+    # if nan set to zero also
+    idpolrad = np.where(np.isnan(idpolrad), 0, idpolrad)
 
-    elif version == 'modified':
-        HV_target = image[1, ...]  # test window
-        HV_clutter = fast_edge_mean(image[1, ...], mask)  # train window
-        HH_clutter = fast_edge_mean(image[0, ...], mask)  # train window
+    # estimate the parameters for the generalized gamma distribution
+    # exclude values smaller than 50 x mean and larger than 0
+    samples = idpolrad[np.where((idpolrad < 50 * np.mean(idpolrad)) & (idpolrad > 0))]
+    gengamma_params = scipy.stats.gengamma.fit(samples)
 
-    HH_clutter = np.where(HH_clutter == 0, np.nan, HH_clutter)  # dont divide by 0
-    transform = (HV_target - HV_clutter) / (HH_clutter)
+    # calculate the threshold corresponding to the desired PFA
+    pde = 1 - pfa
+    init = 10 * np.mean(idpolrad)  # initial guess of the algorithm
+    T = scipy.optimize.fmin(_gengamma_minimize, init, disp=False, args=(gengamma_params, pde), ftol=1e-21)[0]
 
-    transform = mask_edges(transform, 7, np.nan)
+    outliers = (idpolrad > T)
 
-    return transform
+    return outliers
