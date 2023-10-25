4b93fa2cc593a70144ee028ad01ba769f991f2aa
LaustFaerch <laust.faerch@gmail.com>
2023-10-25 11:58:06 +0200

    add dynamic winwdows for all detectors

--- cfar_filters/detector.py
+++ cfar_filters/detector.py
@@ -30,31 +30,33 @@ import numpy as np
 from . import kdistribution, lognormal, wishart, normsum, dpolrad, gamma, utils
 # TODO: Add check for method == 'AND' or 'OR'
 
-def run(image, mask, detector='gamma', method='AND', pfa=1e-9, enl=10.7, minsize=2, sensitivity=40):
+# TODO: add checks: window sizes must be correct size and positive.
+
+def run(image, mask, detector='gamma', method='AND', pfa=1e-9, enl=10.7, minsize=2, sensitivity=40, wi=9, wo=15):
 
     if detector == 'gamma':
         image = utils.db2in(image)
 
         if method == 'AND':
-            hh_outliers = gamma.detector(image[0, ...], mask=mask, pfa=np.sqrt(pfa), enl=enl)
-            hv_outliers = gamma.detector(image[1, ...], mask=mask, pfa=np.sqrt(pfa), enl=enl)
+            hh_outliers = gamma.detector(image[0, ...], mask=mask, pfa=np.sqrt(pfa), enl=enl, wi=wi, wo=wo)
+            hv_outliers = gamma.detector(image[1, ...], mask=mask, pfa=np.sqrt(pfa), enl=enl, wi=wi, wo=wo)
             outliers = hh_outliers & hv_outliers
 
         elif method == 'OR':
-            hh_outliers = gamma.detector(image[0, ...], mask=mask, pfa=1 - np.sqrt(1 - pfa), enl=enl)
-            hv_outliers = gamma.detector(image[1, ...], mask=mask, pfa=1 - np.sqrt(1 - pfa), enl=enl)
+            hh_outliers = gamma.detector(image[0, ...], mask=mask, pfa=1 - np.sqrt(1 - pfa), enl=enl, wi=wi, wo=wo)
+            hv_outliers = gamma.detector(image[1, ...], mask=mask, pfa=1 - np.sqrt(1 - pfa), enl=enl, wi=wi, wo=wo)
             outliers = hh_outliers | hv_outliers
 
     elif detector == 'lognorm':
 
         if method == 'AND':
-            hh_outliers = lognormal.detector(image[0, ...], mask=mask, pfa=np.sqrt(pfa))
-            hv_outliers = lognormal.detector(image[1, ...], mask=mask, pfa=np.sqrt(pfa))
+            hh_outliers = lognormal.detector(image[0, ...], mask=mask, pfa=np.sqrt(pfa), wi=wi, wo=wo)
+            hv_outliers = lognormal.detector(image[1, ...], mask=mask, pfa=np.sqrt(pfa), wi=wi, wo=wo)
             outliers = hh_outliers & hv_outliers
 
         elif method == 'OR':
-            hh_outliers = lognormal.detector(image[0, ...], mask=mask, pfa=1 - np.sqrt(1 - pfa))
-            hv_outliers = lognormal.detector(image[1, ...], mask=mask, pfa=1 - np.sqrt(1 - pfa))
+            hh_outliers = lognormal.detector(image[0, ...], mask=mask, pfa=1 - np.sqrt(1 - pfa), wi=wi, wo=wo)
+            hv_outliers = lognormal.detector(image[1, ...], mask=mask, pfa=1 - np.sqrt(1 - pfa), wi=wi, wo=wo)
             outliers = hh_outliers | hv_outliers
 
     elif detector == 'k':
@@ -62,31 +64,32 @@ def run(image, mask, detector='gamma', method='AND', pfa=1e-9, enl=10.7, minsize
 
         if method == 'AND':
             hh_outliers = kdistribution.detector(image[0, ...], mask=mask, N=sensitivity,
-                                                 pfa=np.sqrt(pfa), enl=enl)
+                                                 pfa=np.sqrt(pfa), enl=enl, wi=wi, wo=wo)
             hv_outliers = kdistribution.detector(image[1, ...], mask=mask, N=sensitivity,
-                                                 pfa=np.sqrt(pfa), enl=enl)
+                                                 pfa=np.sqrt(pfa), enl=enl, wi=wi, wo=wo)
             outliers = hh_outliers & hv_outliers
 
         elif method == 'OR':
             hh_outliers = kdistribution.detector(image[0, ...], mask=mask, N=sensitivity,
-                                                 pfa=1 - np.sqrt(1 - pfa), enl=enl)
+                                                 pfa=1 - np.sqrt(1 - pfa), enl=enl, wi=wi, wo=wo)
             hv_outliers = kdistribution.detector(image[1, ...], mask=mask, N=sensitivity,
-                                                 pfa=1 - np.sqrt(1 - pfa), enl=enl)
+                                                 pfa=1 - np.sqrt(1 - pfa), enl=enl, wi=wi, wo=wo)
             outliers = hh_outliers | hv_outliers
 
     elif detector == 'wishart':
         image = utils.db2in(image)
-        outliers = wishart.detector(image, mask=mask, pfa=pfa, enl=enl)
+        outliers = wishart.detector(image, mask=mask, pfa=pfa, enl=enl, wi=wi, wo=wo)
 
     elif detector == 'nis':
         image = utils.db2in(image)
         nis_transform = normsum.transform(image, mask=mask)
         nis_enl = utils.calc_enl(np.where(nis_transform < np.nanmedian(nis_transform) * 2, nis_transform, np.nan))
-        outliers = gamma.detector(nis_transform, mask=mask, pfa=pfa, enl=nis_enl)
+        outliers = gamma.detector(nis_transform, mask=mask, pfa=pfa, enl=nis_enl, wi=wi, wo=wo)
 
     elif detector == 'idpolrad':
         image = utils.db2in(image)
-        outliers = dpolrad.detector(image, mask=mask, pfa=pfa)
+
+        outliers = dpolrad.detector(image, mask=mask, pfa=pfa, wi=wi, wo=wo)
 
     else:
         warnings.warn(f'Detector not recognized. Use one of the following: \
