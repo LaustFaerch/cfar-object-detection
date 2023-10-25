cf66631286503b9d001040340cfa3e21e8364bc7
LaustFaerch <laust.faerch@gmail.com>
2023-10-25 11:40:04 +0200

    dynamic window sizes for normum detector

--- cfar_filters/normsum.py
+++ cfar_filters/normsum.py
@@ -3,7 +3,7 @@ import numpy as np
 from .utils import smells_like, mask_edges
 from .fast_functions import fast_edge_mean
 
-def transform(image, mask=0):
+def transform(image, mask=0, wi=9, wo=15):
     """
     Normalized Intensity Sum transformation (NIS).
     Based on the normalized intensity sum decision variable suggested by Chen Liu in:
@@ -19,6 +19,10 @@ def transform(image, mask=0):
         SAR image in linear intensity format
     mask : numpy.ndarray(bool) (X,Y)
         Mask for the image.
+    wi : integer
+        Inner Window Diameter - also called guard area
+    wo : integer
+        Outer Window Diameter - also called clutter estimation area
 
     Returns:
     ----------
@@ -48,14 +52,24 @@ def transform(image, mask=0):
         warnings.warn(f'Input image should be in intensity scale. Image smells like {smells_like(image)}',
                       category=UserWarning)
 
-    HH_edge = fast_edge_mean(image[0, ...], mask)
-    HV_edge = fast_edge_mean(image[1, ...], mask)
+    # check that window sizes are valid
+    if wi > wo:
+        raise ValueError((f'Outer window must be larger than inner window \
+                          wi: {wi}, wo {wo}'))
+    if wo > 20:
+        raise ValueError((f'Maximum allowable window size is 20. \
+                          If you want larger windows, you should change the neighbourhood and ranges in fast_functions.\
+                           But be aware complexity increases with the square of the neighborhood size. \
+                          wo {wo}'))
+
+    HH_edge = fast_edge_mean(image[0, ...], mask, wi, wo)
+    HV_edge = fast_edge_mean(image[1, ...], mask, wi, wo)
 
     HH_edge = np.where(HH_edge == 0, np.nan, HH_edge)
     HV_edge = np.where(HV_edge == 0, np.nan, HV_edge)
 
     NIS = (image[0, ...] / HH_edge) + (image[1, ...] / HV_edge)
 
-    transform = mask_edges(NIS, 7, np.nan)
+    transform = mask_edges(NIS, 20, np.nan)
 
     return transform
